58974014c0344135db11eeb1e68b31303323f1bd
Jeff Casimir <jeff@casimircreative.com>
2013-09-27 11:44:46 -0600

    Implemented a Half-Adder

---
+++ lib/adders/half.rb
@@ -0,0 +1,19 @@
+require './lib/signals'
+require './lib/gates/xor'
+require './lib/gates/and'
+
+module Adders
+  class Half
+    attr_accessor :input_a, :input_b
+
+    include Hardware::Signals
+
+    def sum
+      Gates::Xor.new(input_a: input_a, input_b: input_b).output
+    end
+
+    def carry
+      Gates::And.new(input_a: input_a, input_b: input_b).output
+    end
+  end
+end
