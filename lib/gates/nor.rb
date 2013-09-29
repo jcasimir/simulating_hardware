aa1212331c9c09f63b7d474c4aa0f38c821a2648
Paul Simpson <prsimp@gmail.com>
2013-09-29 16:41:54 -0400

    Added initializer to NOR gate

--- lib/gates/nor.rb
+++ lib/gates/nor.rb
@@ -7,6 +7,11 @@ module Gates
 
     include Hardware::Signals
 
+    def initialize(inputs = {})
+      @input_a = inputs[:input_a]
+      @input_b = inputs[:input_b]
+    end
+
     def output
       or_gate = Or.new(input_a: input_a, input_b: input_b)
       not_gate = Not.new(or_gate.output)
