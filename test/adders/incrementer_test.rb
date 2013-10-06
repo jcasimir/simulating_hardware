3623e7c97fe2c92ee12983c72ef4d8572f4011ce
Jeff Casimir <jeff@jumpstartlab.com>
2013-10-06 07:37:56 -0700

    First draft of incrementer

---
+++ test/adders/incrementer_test.rb
@@ -0,0 +1,19 @@
+require './test/test_helper'
+require './lib/adders/incrementer'
+
+class IncrementerTest < GateTest
+  def incrementer
+    @incrementer ||= Adders::Incrementer.new(4)
+  end
+
+  def test_it_increments_zero_to_one
+    incrementer.input = Helpers::Binary.new('0000')
+    assert_equal Helpers::Binary.new('0001'), incrementer.output
+  end
+
+  def test_it_overflows_without_complaint
+    skip
+    incrementer.input = Helpers::Binary.new('1111')
+    assert_equal Helpers::Binary.new('0000'), incrementer.output
+  end
+end
