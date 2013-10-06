8f1e8850f74f4cdb3e588ff48e096c5e461e9cc4
Jeff Casimir <jeff@casimircreative.com>
2013-10-06 07:37:56 -0700

    First draft of incrementer

--- lib/adders/incrementer.rb
+++ lib/adders/incrementer.rb
@@ -1,16 +1,36 @@
 require './lib/helpers/signals'
+require './lib/helpers/binary'
 require './lib/adders/full'
 
 module Adders
   class Incrementer
     attr_accessor :input
+    attr_reader   :size
 
-    def initialize(bits)
-
+    def initialize(bit_size)
+      @size = bit_size
     end
 
     def output
-      Helpers::Binary.new("0000 0001")
+      adder.output
+    end
+
+  private
+    def adder
+      @adder ||= build_adder
+    end
+
+    def build_adder
+      adder = Adders::Ripple.new(size)
+      adder.input_a = input
+      adder.input_b = increment_by
+      adder
+    end
+
+    def increment_by
+      reversed_bits = Array.new(size){ 0 }
+      reversed_bits[-1] = 1
+      Helpers::Binary.from_bits(reversed_bits)
     end
 
   end
