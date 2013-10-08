d8522e77492aacd13d47d5d329398def1cfae0b7
Jeff Casimir <jeff@jumpstartlab.com>
2013-10-07 21:18:10 -0600

    Remove sample tests from multibit multiplexer

--- test/mux/multibit_multiplexer_test.rb
+++ test/mux/multibit_multiplexer_test.rb
@@ -40,32 +40,4 @@ class MultiplexerTest < GateTest
     mux.control = Helpers::Binary.new('11')
     assert_high mux.output
   end
-
-  # def test_high_a_and_low_control_outputs_high
-  #   mux.input_a = HIGH
-  #   mux.input_b = LOW
-  #   mux.control = LOW
-  #   assert_high mux.output
-  # end
-
-  # def test_high_b_and_low_control_outputs_low
-  #   mux.input_a = LOW
-  #   mux.input_b = HIGH
-  #   mux.control = LOW
-  #   assert_low mux.output
-  # end
-
-  # def test_low_b_and_high_control_outputs_low
-  #   mux.input_a = LOW
-  #   mux.input_b = LOW
-  #   mux.control = HIGH
-  #   assert_low mux.output
-  # end
-
-  # def test_high_b_and_high_control_outputs_high
-  #   mux.input_a = LOW
-  #   mux.input_b = HIGH
-  #   mux.control = HIGH
-  #   assert_high mux.output
-  # end
 end
