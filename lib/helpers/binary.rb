592b990ff52f7647aed80b17b9c0d6119b11340c
Jeff Casimir <jeff@casimircreative.com>
2013-10-17 10:49:17 -0600

    Added more functionality to Binary helper to assist in ALU testing

--- lib/helpers/binary.rb
+++ lib/helpers/binary.rb
@@ -4,7 +4,10 @@ module Helpers
 
     attr_reader :value
 
-    def initialize(input)
+    def initialize(input, bit_size = nil)
+      if bit_size
+        input = input.rjust(bit_size, "0")
+      end
       @value = input
     end
 
@@ -21,11 +24,54 @@ module Helpers
     end
 
     def ==(other)
+      unless other.respond_to?(:bits)
+        raise "Can't compare #{self.inspect} to #{other.inspect} (#{other.class})"
+      end
       self.bits == other.bits
     end
 
     def size
       bits.size
+    end
+
+    def to_decimal
+      if bits.last == 1
+        (self.inverse + Binary.new("1", self.size)).to_decimal * -1
+      else
+        value.to_i(2)
+      end
+    end
+
+    def inverse
+      bits = value.scan(/\d/).collect do |original|
+        original == "1" ? "0" : "1"
+      end.join
+      Binary.new(bits)
+    end
+
+    def +(other)
+      unless self.size == other.size
+        raise ArgumentError.new("numbers to be added must have the same number of bits")
+      end
+
+      bit_pairs = bits.zip(other.bits)
+      carry = 0
+      result = bit_pairs.collect do |a, b|
+        if a + b + carry == 3
+          carry = 1
+          1
+        elsif a + b + carry  == 2
+          carry = 1
+          0
+        elsif a + b + carry == 1
+          carry = 0
+          1
+        else
+          carry = 0
+          0
+        end
+      end
+      Binary.new(result.reverse.join)
     end
 
   private
