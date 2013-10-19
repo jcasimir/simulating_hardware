63eb07f2fd836d0a9d3bef06c5a93f91d712bdd4
Jeff Casimir <jeff@casimircreative.com>
2013-10-19 02:08:23 -0600

    Create and use a multi-bit inverter

---
+++ lib/gates/inverter.rb
@@ -0,0 +1,38 @@
+require './lib/helpers/signals'
+require './lib/gates/nand'
+
+module Gates
+  class Inverter
+    attr_accessor :input
+    attr_reader   :size
+
+    include Helpers::Signals
+
+    def initialize(size)
+      @size = size
+    end
+
+   def output
+      Helpers::Binary.from_bits output_bits
+    end
+
+  private
+
+    def output_bits
+      gates.collect{|g| g.output}.reverse
+    end
+
+    def gates
+      @gates ||= build_gates
+    end
+
+    def build_gates
+      (0...size).collect do |bit|
+        gate = Gates::Nand.new
+        gate.input_a = lambda{ input.bits[bit] }
+        gate.input_b = lambda{ input.bits[bit] }
+        gate
+      end
+    end
+  end
+end
